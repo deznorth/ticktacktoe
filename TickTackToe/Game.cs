d9f370c43be137da9a0b61900c215d6db8c19846
David Rojas <deznorth@gmail.com>
2019-05-16 14:20:01 -0400

    Contd work on Board class

--- TickTackToe/Game.cs
+++ TickTackToe/Game.cs
@@ -8,8 +8,37 @@ namespace TickTackToe
         {
             Console.WriteLine("Hello World!");
             Board board = new Board();
-            Console.WriteLine(board.Cols);
             Console.ReadKey();
+        }
+    }
+
+    struct State //IMPORTANT! continue this.
+    {
+        //GameState - keeps track of the currentBoard and other game parameters
+        private Board _currentBoard;
+        private int _round;
+        private int _currentPlayer;
+        private bool _isGameOver;
+
+        //BoardState - keeps track of the content of each Box
+        private Box[][] _boxes;
+
+        private void gameInit()
+        {
+            _currentBoard = new Board();
+            _round = 1;
+            _currentPlayer = 1;
+            _isGameOver = false;
+        }
+    }
+
+    struct Box
+    {
+        private int _x, _y;
+
+        public int[] position
+        {
+            get => { _x, _y};
         }
     }
 
