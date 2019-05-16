1a21a766535d74e119761176fdbb7f999258e8a7
David Rojas <deznorth@gmail.com>
2019-05-15 22:57:56 -0400

    Created Board Class constructor

--- TickTackToe/Game.cs
+++ TickTackToe/Game.cs
@@ -7,6 +7,32 @@ namespace TickTackToe
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Board board = new Board();
+            Console.WriteLine(board.Cols);
+            Console.ReadKey();
+        }
+    }
+
+    class Board
+    {
+        private int rows, cols;
+
+        public Board(int rows = 3, int cols = 3)
+        {
+            this.rows = rows;
+            this.cols = cols;
+        }
+
+        public int Rows
+        {
+            get => rows;
+            set => rows = value;
+        }
+
+        public int Cols
+        {
+            get => cols;
+            set => cols = value;
         }
     }
 }
